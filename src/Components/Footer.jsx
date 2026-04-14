f20b465cb37b0859d3d38dd559ea78e56dc58b76
anthonykravchishin <tonykravc53@gmail.com>
2026-04-14 11:29:30 -0700

    Added footer styles

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,11 +1,14 @@
+import "../styles/footer.css";
 export default function Footer() {
 	return (
 		<>
-			<p id="footer-logo">LOGO</p>
-			<div id="footer-layout">
-				<p>Ahmed Ihsan and Anthony Kravchishin</p>
-				<p>Privacy Policy</p>
-				<p>Terms of Service</p>
+            <div id="footer-layout">
+                <p id="footer-logo">LOGO</p>
+                <div id="footer-misc">
+                    <p>Ahmed Ihsan and Anthony Kravchishin</p>
+                    <p>Privacy Policy</p>
+                    <p>Terms of Service</p>
+                </div>
 			</div>
 		</>
 	);
