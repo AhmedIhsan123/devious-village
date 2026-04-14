daa7112ac28eda1bb39f8c07ac8ed163aab4e912
anthonykravchishin <tonykravc53@gmail.com>
2026-04-14 10:59:22 -0700

    Added header and history

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import GameCard from "./Components/GameCard.jsx";
+import Header from "./Components/Header.jsx";
+import History from "./Components/History.jsx";
 export default function () {
 	return (
 		<>
+      <Header />
+      <History />
 			<GameCard></GameCard>
 		</>
 	);
---
+++ src/Components/History.jsx
@@ -0,0 +1,13 @@
+export default function History() {
+    return <>
+        <div id="history-layout">
+            <h1>History of Resident Evil</h1>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eligendi numquam vero perspiciatis eveniet assumenda asperiores, fugit dignissimos adipisci quo libero laborum architecto atque sequi aliquid eius in dolores incidunt ullam.</p>
+            <div id="history-links">
+                <a href="#">View Wiki</a>
+                <a href="#">View Store</a>
+            </div>
+            <img src=""></img>
+        </div>
+    </>
+}
